22ec1fd4b53f455a28608f117a129897e2cf8e4a
theeurbanlegend <mbuto152@gmail.com>
2023-06-08 10:36:06 +0300

    Create element,Adding  and clearing input

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 let myLeads=[];
 const InputEl=document.getElementById("input-el")
 const InputBtn=document.getElementById("input-btn")
-InputBtn.addEventListener("click", () => {
-    
-    myLeads.push(document.getElementById("input-el").value)
-    console.log(myLeads)    
+const unorderedList=document.getElementById("unordered")
+
+InputBtn.addEventListener("click", () => {   
+    myLeads.push(InputEl.value)
+    InputEl.value=""
+    displayLeads()
     })
-    
+
+function displayLeads(){
+    let listItems=""
+    for(let i=0; i< myLeads.length; i++){
+        listItems+="<li>" +myLeads[i]+"</li>"
+     }
+    unorderedList.innerHTML=listItems
+}
