d86bb052c926078b655c6ec65580131fe54e0bf7
theeurbanlegend <mbuto152@gmail.com>
2023-06-08 13:59:26 +0300

    Local storage add

--- main.js
+++ main.js
@@ -4,13 +4,23 @@ const InputEl=document.getElementById("input-el")
 const InputBtn=document.getElementById("input-btn")
 const unorderedList=document.getElementById("unordered")
 
+
 InputBtn.addEventListener("click", () => {   
     myLeads.push(InputEl.value)
     InputEl.value=""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     displayLeads()
-    })
 
+    
+    })
+let leadsFromStorage=JSON.parse(localStorage.getItem("myLeads"))
+if (leadsFromStorage){
+   myLeads=leadsFromStorage
+   displayLeads()
+}
+console.log(leadsFromStorage)
 function displayLeads(){
+    
     let listItems=""
     for(let i=0; i< myLeads.length; i++){
         listItems+=
